da6ea33b8fc0d938e45123ec40ddebb25b0ffc20
kimjh <kjh05505@gmail.com>
2026-01-09 14:36:33 +0900

    modify: gitignore
    delete Icon ignore

---
+++ Example/Design/Sources/Icon/Icons.swift
@@ -0,0 +1,19 @@
+//
+//  Icons.swift
+//  Design
+//
+//  Created by 김재한 on 10/1/25.
+//
+
+import Foundation
+import UIKit
+
+public enum Icons {
+    public static let notice = UIImage(resource: .imgNotice)
+    public static let heart = UIImage(resource: .imgHeart16)
+    public static let pin = UIImage(resource: .imgPin)
+    public static let clock = UIImage(resource: .imgClock)
+    public static let defaultThumbnail = UIImage(resource: .imgDefaultThumbnail)
+    public static let topNavigationLeft24 = UIImage(resource: .iconTopnavigationLeft24)
+    public static let topNavigationUp24 = UIImage(resource: .iconTopnavigationUp24)
+}
